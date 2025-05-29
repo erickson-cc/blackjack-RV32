62c7ed3652e646b2c117dbf934396f9024f6b3d7
erickson-cc <e.rickson@live.com>
2025-05-28 22:02:07 -0300

    feat: Inserção de quebra de linhas nos labels de recebe_player

--- blackjack.asm
+++ blackjack.asm
@@ -25,22 +25,25 @@ teste_qtd:		.string	" existe em quantidade: "
 	li s9, 52 # usado como count do teste
 	li s11, 1# Define o valor 1 para o registrador s11, usado para manusear escolhas
 	j inicio
+
 escolha_comando:
 	# Solicita input do comando
 	li a7, 5
 	ecall
 	mv t6, a0
 	ret
+
 dar_carta:
 	la t0, baralho_qtd
 	ret
+
 sortear:
 	# Sorteia o índice de 0-12
 	li a0, 0
 	li a1, 13
 	li a7, 42 # RandIntRange
 	ecall
-
+	#
 	# verifica se ainda há cartas desse valor
 	add t1, t0, a0
 	lb t2, 0(t1) #--- Load
@@ -50,6 +53,7 @@ sortear:
 	addi t2, t2, -1
 	sb t2, 0(t1) #--- Store
 	ret
+
 inicio:	
 	#esses tres abaixo vão ser movidos para outro estado
 	call dar_carta
@@ -57,32 +61,52 @@ inicio:
 	call imprimir_teste
 	addi s9, s9, -1
 	bne s9, zero, inicio
-	
+	#
 	#Fim dos testes, interface começa aqui
 	la a0, inicio_instr
 	li a7, 4
 	ecall
-
+	#
 	jal escolha_comando
 	beq t6, zero, end
 	beq t6, s11, player_recebe_1 
 	j inicio	
+
 player_recebe_1:
 	# Adicionar carta a player.mao
 	la a0, player_recebe
 	li a7, 4
 	ecall
+	#
+	# incluir lógica de receber carta aqui (talvez um JAL)
+	#imprime quebra de linha (ASCII 10)
+	li a0, 10
+	li a7, 11
+	ecall
+
 player_recebe_2:
 	# Adicionar carta a player.mao
 	la a0, player_recebe
 	li a7, 4
 	ecall
+	# incluir lógica de receber carta aqui (talvez um JAL)
+		#imprime quebra de linha (ASCII 10)
+		li a0, 10
+		li a7, 11
+		ecall
 	j player_escolha
+
 player_hit:
 	# Adicionar carta a player.mao
 	la a0, player_recebe
 	li a7, 4
 	ecall
+	# incluir lógica de receber carta aqui (talvez um JAL)
+		#imprime quebra de linha (ASCII 10)
+		li a0, 10
+		li a7, 11
+		ecall
+
 player_escolha:
 	la a0, escolha
 	li a7, 4
@@ -96,12 +120,14 @@ player_stand:
 	li a7, 4
 	ecall
 	j dealer_escolha
+
 dealer_escolha:
 	la a0, vez_do_dealer
 
 end:
 	li a7, 10
 	ecall
+
 imprimir_teste:
 	mv s8, a0 # Salva o índice para poder usar o a0
 	# Imprimir texto
@@ -123,9 +149,8 @@ imprimir_teste:
 	lb a0, 0(t2)	# a0 = valor do baralho_qtd[a0]
 	li a7, 1
 	ecall
-	#imprime quebra de linha (ASCII 10)
-	li a0, 10
-	li a7, 11
-	ecall
+		#imprime quebra de linha (ASCII 10)
+		li a0, 10
+		li a7, 11
+		ecall
 	ret
-	
